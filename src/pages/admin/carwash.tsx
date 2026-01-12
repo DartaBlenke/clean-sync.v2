b8de9e4a309c34988d193803b1578aacbe6a748f
D`Artagnan Blenke <dartagnan.blenke17@gmail.com>
2026-01-12 00:08:56 +0000

    carwash page and customer page

---
+++ src/pages/admin/carwash.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CarwashPage = () => {
+  return <h1>Carwash conf page</h1>;
+};
+
+export default CarwashPage;
---
+++ src/pages/customer/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CustomerPage = () => {
+  return <h1>Customer CleanSync</h1>;
+};
+
+export default CustomerPage;
